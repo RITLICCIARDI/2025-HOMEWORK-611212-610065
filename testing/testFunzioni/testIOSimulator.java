a583d3583cd9e543561f2ae1007f49de43237885
Yaskaaasksk <may.morasca@stud.uniroma3.it>
2025-05-10 20:02:23 +0200

    modifica testIOSimulator

--- testing/testFunzioni/testIOSimulator.java
+++ testing/testFunzioni/testIOSimulator.java
@@ -1,13 +1,10 @@
 package testFunzioni;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import it.uniroma3.diadia.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.BeforeEach;
-
 
 class testIOSimulator {
 
